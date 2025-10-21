15d122e6a77889be3b6d2bf49b9a974587ba9c0b
Vladislav Ainshtein <vladislav.ainshtein@gmail.com>
2025-10-21 19:38:12 +0200

    Initial Commit

---
+++ Sona/SonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonaApp.swift
+//  Sona
+//
+//  Created by Vladislav Ainshtein on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
